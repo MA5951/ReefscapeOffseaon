2cd48686e6fc765a6d4a3e510668d90ddc48f9dd
AlmaKaplan <alma20062010@gmail.com>
2025-09-11 18:43:27 +0300

    added functions in SuperStructure
    setScoringPreset
    getScoringPreset
    istFrontSensore
    isRearSensore
    isAlgea
    isCoral
    isGamePiece
    setScoringLocation

--- src/main/java/frc/robot/RobotControl/SuperStructure.java
+++ src/main/java/frc/robot/RobotControl/SuperStructure.java
@@ -1,4 +1,49 @@
 
 package frc.robot.RobotControl;
 
-public class SuperStructure {}
+import com.MAutils.Logger.MALog;
+
+import frc.robot.RobotConstants;
+import frc.robot.RobotContainer;
+import frc.robot.RobotControl.Field.ScoringLocation;
+
+public class SuperStructure {
+
+    private static Field.ScoringLevel scoringLevel;
+    private static Field.ScoringLocation scoringLocation;
+
+
+    public static void setScoringPreset(Field.ScoringLevel ScoringLevel) {
+        scoringLevel = ScoringLevel;
+    }
+
+    public static Field.ScoringLevel getScoringPreset() {
+        return scoringLevel;
+    }
+
+    public static final boolean istFrontSensore() {
+        return true;
+    }
+
+    public static final boolean isRearSensore() {
+        return true;
+    }
+
+    public static final boolean isAlgea() {
+        return true;
+    }
+
+    public static final boolean isCoral() {
+        return isRearSensore() || istFrontSensore();
+    }
+
+    public static final boolean isGamePiece() {
+        return isCoral() || isAlgea();
+    }
+
+    public static void setScoringLocation(Field.ScoringLocation ScoringLocation) {
+        scoringLocation = ScoringLocation;
+        //MALog("/SuperStructure/Scoring Location", ScoringLocation.name());
+    }
+
+}
