d9322a3dc6a2533760b599f34f1729444c8aac09
AmitayCarmi <amitaycarmi@gmail.com>
2025-09-14 18:13:40 +0300

    fixed states
    removed ball sorting
    added ball prescoring
    fixed eject
    fixed ball intake
    fixed ball holding
    fixed ball scoring

--- src/main/java/frc/robot/RobotConstants.java
+++ src/main/java/frc/robot/RobotConstants.java
@@ -17,75 +17,75 @@ import frc.robot.Subsystem.Intake.IntakeConstants;
 
 public class RobotConstants {
 
-     //TODO:Add ball pre scoring
+    // TODO:Add ball pre scoring
 
     public static final RobotState IDLE = new RobotState(
-        "IDLE", 
-        IntakeConstants.IDLE,
-        ArmConstants.IDLE,
-        ElevatorConstants.IDLE,
-        ClimbConstants.IDLE );
-    
+            "IDLE",
+            IntakeConstants.IDLE,
+            ArmConstants.IDLE,
+            ElevatorConstants.IDLE,
+            ClimbConstants.IDLE);
+
     public static final RobotState INTAKE = new RobotState(
-        "INTAKE",
-        IntakeConstants.CORAL_FORWARD,
-        ArmConstants.INTAKE,
-        ElevatorConstants.INTAKE);
+            "INTAKE",
+            IntakeConstants.CORAL_INTAKE,
+            ArmConstants.INTAKE,
+            ElevatorConstants.INTAKE);
 
     public static final RobotState CORAL_HOLD = new RobotState(
-        "CORAL_HOLD",
-        IntakeConstants.CORAL_HOLD,
-        ArmConstants.CORAL_HOLDING,
-        ElevatorConstants.HOLD);
+            "CORAL_HOLD",
+            IntakeConstants.CORAL_HOLD,
+            ArmConstants.CORAL_HOLDING,
+            ElevatorConstants.HOLD);
 
     public static final RobotState SCORING = new RobotState(
-        "SCORING",
-        IntakeConstants.CORAL_FORWARD,
-        ArmConstants.CORAL_SCORING,
-        ElevatorConstants.SCORING);
-  
+            "SCORING",
+            IntakeConstants.CORAL_SCORING,
+            ArmConstants.CORAL_SCORING,
+            ElevatorConstants.SCORING);
+
     public static final RobotState EJECT = new RobotState(
-        "EJECT",
-        IntakeConstants.CORAL_EJECT);
+            "EJECT",
+            IntakeConstants.EJECT);
 
     public static final RobotState BALL_INTAKE = new RobotState(
-        "BALL_INTAKE",
-        IntakeConstants.BALL_FORWARD,
-        ArmConstants.BALL_INTAKE,
-        ElevatorConstants.BALL_INTAKE);
-        public static final RobotState BALL_SORTING = new RobotState(
-            "BALL_SORTING",
-            IntakeConstants.BALL_SORTING,
-            ArmConstants.BALL_SORTING);
+            "BALL_INTAKE",
+            IntakeConstants.BALL_INTAKE,
+            ArmConstants.BALL_INTAKE,
+            ElevatorConstants.BALL_INTAKE);
 
-    public static final RobotState BALL_HOLDING  = new RobotState(
-        "BALL_HOLDING",
-        IntakeConstants.BALL_HOLD,
-        ArmConstants.BALL_HOLDING,
-        ElevatorConstants.BALL_HOLDING);
+    public static final RobotState BALL_HOLDING = new RobotState(
+            "BALL_HOLDING",
+            IntakeConstants.BALL_HOLD,
+            ArmConstants.BALL_HOLDING,
+            ElevatorConstants.BALL_HOLDING);
+
+    public static final RobotState BALL_PRESCORING = new RobotState(
+            "BALL_PRESCORING",
+            ElevatorConstants.BALL_PRESCORING);
 
     public static final RobotState BALL_SCORING = new RobotState(
-        "BALL_SCORING",
-        IntakeConstants.BALL_BACKWARD,
-        ArmConstants.BALL_SCORING,
-        ElevatorConstants.BALL_SCORING);
+            "BALL_SCORING",
+            IntakeConstants.BALL_SCORING,
+            ArmConstants.BALL_SCORING,
+            ElevatorConstants.BALL_SCORING);
 
-    public static final RobotState PRECLIMBING = new RobotState("PRECLIMBING",
-        IntakeConstants.IDLE,
-        ArmConstants.IDLE,
-        ElevatorConstants.CLOSE,
-        ClimbConstants.OPEN);
+    public static final RobotState PRECLIMBING = new RobotState(
+            "PRECLIMBING",
+            IntakeConstants.IDLE,
+            ArmConstants.IDLE,
+            ElevatorConstants.CLOSE,
+            ClimbConstants.OPEN);
 
-    public static final RobotState CLIMBING = new RobotState("PRECLIMBING",
-        IntakeConstants.IDLE,
-        ArmConstants.IDLE,
-        ElevatorConstants.CLOSE,
-        ClimbConstants.CLOSE);
+    public static final RobotState CLIMBING = new RobotState(
+            "PRECLIMBING",
+            IntakeConstants.IDLE,
+            ArmConstants.IDLE,
+            ElevatorConstants.CLOSE,
+            ClimbConstants.CLOSE);
 
     public static final RobotState SORTING = new RobotState(
-        "SORTING", 
-        IntakeConstants.CORAL_SORTING);
+            "SORTING",
+            IntakeConstants.CORAL_SORTING);
 
-
-    
 }
